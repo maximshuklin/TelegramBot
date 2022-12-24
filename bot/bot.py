7594d34a79e2d9aa10fda74d4a8fffee335d251d
Maksim Shuklin Remote <maximshuklin@mail.ru>
2022-12-24 22:37:51 +0000

    update

--- bot/bot.py
+++ bot/bot.py
@@ -21,7 +21,6 @@ dp = Dispatcher(bot)
 
 
 # States declaration
-
 class State:
 	START =           0
 	HELP =            1
@@ -29,6 +28,7 @@ class State:
 	SEND_MEDIA =      3
 	ADD_CATEGORY =    4
 	DELETE_CATEGORY = 5
+	SHOW_PHOTOS     = 7
 
 
 user_state = {
@@ -48,14 +48,6 @@ def get_user_state(user_id: int):
 		user_state[user_id] = State.START
 	return user_state[user_id]
 
-
-# MAP declatation
-
-MAP = {
-	
-}
-
-
 # a bag of shit
 
 @dp.message_handler(commands=['start'])
@@ -74,41 +66,46 @@ async def process_help_command(message: types.Message):
 	await message.reply(help_message)
 
 
-@dp.message_handler(commands=['add_category'])
+@dp.message_handler(commands=['add', 'add_category'])
 async def process_add_category_command(message: types.Message):
 	user_id = message.from_user.id
 	set_state(user_id, State.ADD_CATEGORY)
 	await bot.send_message(user_id, "add_category")
 
 
-@dp.message_handler(commands=['delete_category'])
+@dp.message_handler(commands=['del', 'delete_category'])
 async def process_delete_category_command(message: types.Message):
 	user_id = message.from_user.id
 	set_state(user_id, State.DELETE_CATEGORY)
 	await bot.send_message(user_id, "delete_category")
 
 
-@dp.message_handler(commands=['show'])
+@dp.message_handler(commands=['categories'])
 async def process_show_categories_command(message: types.Message):
 	user_id = message.from_user.id
 	set_state(user_id, State.START)
 
 	user_categories = list_categories(user_id)
 
-	await bot.send_message(user_id, " ".join(user_categories))
+	await bot.send_message(user_id, "\n".join(user_categories))
+
+
+@dp.message_handler(commands=['show_photos'])
+async def process_show_photo_command(message: types.Message):
+	user_id = message.from_user.id
+	await bot.send_message(user_id, "Фотографии из какой категории вам показать?")
+	set_state(user_id, State.SHOW_PHOTOS)
 
 
 @dp.message_handler(content_types=['photo'])
 async def process_photo_command(message):
-	print("process photos")
 	user_id = message.from_user.id
 	if media_messages.get(user_id) == None:
 		media_messages[user_id] = []
 	media_messages[user_id].append(message)
 
-
-
 	set_state(user_id, State.SEND_MEDIA)
+	await bot.send_message(user_id, "В какую категорию вы хотите отправить фото?")
 
 
 
@@ -130,8 +127,8 @@ async def add_category_text_handler(message: types.Message):
 		else:
 			await bot.send_message(user_id, f"Добавили категорию {user_text}")
 
-		# success or fail
-		set_state(user_id, State.START)
+	# success or fail
+	set_state(user_id, State.START)
 
 
 # delete_category_text_handler
@@ -151,8 +148,8 @@ async def delete_category_text_handler(message: types.Message):
 		else:
 			await bot.send_message(user_id, f"Категория {user_text} была удалена")
 
-		# success or fail
-		set_state(user_id, State.START)
+	# success or fail
+	set_state(user_id, State.START)
 
 
 # media text handler
@@ -169,15 +166,39 @@ async def media_text_handler(message: types.Message):
 		await bot.send_message(user_id, f"Категории {user_text} не существует")
 	else:
 		upload_path = f"{media_path}/{user_id}/{user_text}"
-		for media_message in media_messages:
-			file_id = message.photo[-1].file_id
-			media_message.photo[-1].download(destination_file=f"{upload_path}/{file_id}")
+		if media_messages.get(user_id) == None:
+			media_messages[user_id] = []
+
+		for media_message in media_messages.get(user_id):
+			file_id = media_message.photo[-1].file_id
+			await media_message.photo[-1].download(destination_file=f"{upload_path}/{file_id}")
 		await bot.send_message(user_id, f"Фотография загружена в категорию {user_text}!!!")
-		# success
-		set_state(user_id, State.START)
+		
+	set_state(user_id, State.START)
 
 	if media_messages[user_id] != None:
 		media_messages[user_id].clear()
+
+
+async def show_photos_text_handler(message: types.Message):
+	user_id = message.from_user.id
+	user_text = message.text
+
+	user_categories = list_categories(user_id)
+
+	# show photos from user_text category
+
+	if not (user_text in user_categories):
+		await bot.send_message(user_id, f"Категории {user_text} не существует")
+	else:
+		path = f"{media_path}/{user_id}/{user_text}"
+		for photo_name in os.listdir(path):
+			photo_path = f"{path}/{photo_name}"
+			photo_data = open(photo_path, "rb")
+
+			await bot.send_photo(user_id, photo_data)
+
+	set_state(user_id, State.START)
 
 
 async def simple_text_handler(message: types.Message):
@@ -185,8 +206,6 @@ async def simple_text_handler(message: types.Message):
 	user_text = message.text
 
 	await bot.send_message(user_id, f"Нет такой команды {user_text}. Чтобы получить помощь нажмите /help")
-
-
 
 
 
@@ -201,6 +220,8 @@ async def text_handler(message: types.Message):
 		await delete_category_text_handler(message)
 	elif get_user_state(user_id) == State.SEND_MEDIA:
 		await media_text_handler(message)
+	elif get_user_state(user_id) == State.SHOW_PHOTOS:
+		await show_photos_text_handler(message)
 	else:
 		await simple_text_handler(message)
 
